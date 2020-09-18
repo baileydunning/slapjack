a7127d48a839b3470f1296e78485cdba8a3139d1
Bailey Dunning <bwdunning@gmail.com>
2020-09-18 17:07:26 -0600

    Create new instance of Player class on form submission

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-var namePlayer1 = document.querySelector('.name-player1')
-var namePlayer2 = document.querySelector('.name-player2')
-var player1 = document.querySelector('.player1')
-var player2 = document.querySelector('.player2')
+var players = []
+
+var formPlayer1 = document.querySelector('.form-player1')
+var playerOneNameField = document.querySelector('.player1-name')
+var player1info = document.querySelector('.player1-info')
+var formPlayer2 = document.querySelector('.name-player2')
+var playerTwoNameField = document.querySelector('.player2-name')
+var player2info = document.querySelector('.player2-info')
 
 function toggleHidden() {
   for (var i = 0; i < arguments.length; i++) {
@@ -9,18 +13,29 @@ function toggleHidden() {
   }
 }
 
-function displayPlayerOne() {
-  toggleHidden(player1)
-  namePlayer1.classList.add('hidden')
-  var player1Name = document.querySelector('.player1-name')
-  var user1 = document.querySelector('.user1')
-  user1.innerText = player1Name.value
+function createPlayerOne() {
+  toggleHidden(player1info)
+  formPlayer1.classList.add('hidden')
+  var playerOneTitle = document.querySelector('.player1-title')
+  playerOneTitle.innerText = playerOneNameField.value
+  createPlayer(playerOneNameField.value)
 }
 
-function displayPlayerTwo() {
-  toggleHidden(player2)
-  namePlayer2.classList.add('hidden')
-  var player2Name = document.querySelector('.player2-name')
-  var user2 = document.querySelector('.user2')
-  user2.innerText = player2Name.value
+function createPlayerTwo() {
+  toggleHidden(player2info)
+  formPlayer2.classList.add('hidden')
+  var playerTwoTitle = document.querySelector('.player2-title')
+  playerTwoTitle.innerText = playerTwoNameField.value
+  createPlayer(playerTwoNameField.value)
+}
+
+function createPlayer(name) {
+  if (name === playerOneNameField.value) {
+    var player1 = new Player(name)
+    players.unshift(player1)
+  }
+  if (name === playerTwoNameField.value) {
+    var player2 = new Player(name)
+    players.push(player2)
+  }
 }
